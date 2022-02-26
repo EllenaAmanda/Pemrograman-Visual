5246c6d61036002411ac46c807f19de1c1cbbd42
EllenaAmanda <76916918+EllenaAmanda@users.noreply.github.com>
2022-02-27 01:08:57 +0700

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USER
+ * Date: 26/02/2022
+ * Time: 17.46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tugas3
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			string nama = textBox1.Text;
+			string jk = textBox2.Text;
+			string nim = textBox3.Text;
+			string fakultas = textBox4.Text;
+			string jurusan = textBox5.Text;
+			string stambuk = textBox6.Text;
+			
+			string batas = "  ||  ";
+			
+			listbox1.Items.Add(nama+batas+jk+batas+nim+batas+fakultas+batas+jurusan+batas+stambuk);
+			textBox1.Clear();
+			textBox2.Clear();
+			textBox3.Clear();
+			textBox4.Clear();
+			textBox5.Clear();
+			textBox6.Clear();
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			listbox1.Items.RemoveAt(listbox1.SelectedIndex);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
  * Created by SharpDevelop.
  * User: USER
  * Date: 26/02/2022
- * Time: 17.07
+ * Time: 17.20
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
@@ -10,11 +10,15 @@ using System;
 
 namespace tugas1._
 {
-	class Program
+	class ProgramHW
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Nama kamu siapa?");
+			
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama kamu "+nama);
+			Console.Write("Press any key to continue...");
 			Console.ReadKey(true);
 		}
 	}
