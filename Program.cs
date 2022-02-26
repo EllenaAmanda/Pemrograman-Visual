56bc0e7be8b352cc48f07af5a488c50b7e60af16
EllenaAmanda <76916918+EllenaAmanda@users.noreply.github.com>
2022-02-27 01:11:04 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USER
+ * Date: 26/02/2022
+ * Time: 17.07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tugas1._
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey(true);
+		}
+	}
+}
